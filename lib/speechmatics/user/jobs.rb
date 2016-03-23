505112e76a2b11b3bb2573a1e4b2d2fca13236f6
Michal Matyas <github@higher.lv>
2016-03-23 14:35:15 +0100

    Add alignment API support

--- lib/speechmatics/user/jobs.rb
+++ lib/speechmatics/user/jobs.rb
@@ -17,6 +17,11 @@ module Speechmatics
       request(:get, "#{base_path}/transcript")
     end
 
+    def alignment(params={})
+      self.current_options = current_options.merge(args_to_options(params))
+      request(:get, "#{base_path}/alignment")
+    end
+
     def set_mode(params={})
       params[:model] ||= 'en-US'
       params
@@ -30,6 +35,12 @@ module Speechmatics
       content_type = params[:content_type] || MimeMagic.by_path(file_path).to_s
       raise "No content type specified for file, please provide a :content_type value" unless content_type
       raise "Content type for file '#{file_path}' is not audio or video, it is '#{content_type}'." unless (content_type =~ /audio|video/)
+
+      text_file_path = params[:text_file]
+      unless text_file_path.nil?
+        text_content_type = params[:text_content_type] || MimeMagic.by_path(text_file_path).to_s
+        params[:text_file] = Faraday::UploadIO.new(text_file_path, text_content_type)
+      end
 
       params[:data_file] = Faraday::UploadIO.new(file_path, content_type)
       params
